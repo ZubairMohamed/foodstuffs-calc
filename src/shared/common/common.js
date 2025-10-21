0f8c44ecd48ae290a74d140ceef51a2fbc165116
Zubair <zubairnz@outlook.com>
2025-10-21 13:10:40 +1300

    added shared functionality
    shared/common functionality has been added that performs logical operations used by our calculator

---
+++ src/shared/common/common.js
@@ -0,0 +1,45 @@
+import discountRates from "../../data/discounts-rates.json";
+
+/**
+ * This is a dummy function used for testing
+ * @param a
+ * @param b
+ * @returns {*}
+ */
+export function sumTwoNumbers(a, b) {
+  return a + b;
+}
+
+export function getApplicableDiscount(sortedArrayByOrderValue,totalPricePaidByCustomer) {
+  if(sortedArrayByOrderValue === undefined || sortedArrayByOrderValue === null || sortedArrayByOrderValue.length === 0) {
+    return;
+  }
+
+  for(let i = 0; i < sortedArrayByOrderValue.length; i++) {
+    console.log("what not", sortDiscountsByOrderValue, totalPricePaidByCustomer);
+  }
+
+}
+
+/**
+ * This function takes in a dollar amount and converts it back to a number
+ * example input $1,2345.67 should return $1234.67
+ * @param currencyAmount
+ */
+export function convertCurrencyToNumber(currencyAmount) {
+  return parseFloat(currencyAmount.replace(/[^0-9\.]+/g, ""));
+}
+
+/**
+ * This function uses the data contained in the tax rates by region json file and sorts the data based on the order value
+ */
+export function sortDiscountsByOrderValue() {
+  console.log("sorted values: ", discountRates.sort((a, b) => {
+    const convertedValue = (val) => (typeof val === "number" ? val : parseFloat(val));
+
+    return convertedValue(a.OrderValue) - convertedValue(b.OrderValue);
+  }));
+
+}
+
+
